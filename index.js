d4109b829606055fe1ca288520d315f95ade8d25
kumabio <kumabio.prince@student.moringaschool.com>
2022-05-13 06:52:36 +0000

    Assignment Completed

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(blocks){
+    return Math.abs(blocks-42); 
+  };
+  
+  function distanceFromHqInFeet(feet){
+    return (Math.abs(feet-42)*264);
+  };
+  
+  function distanceTravelledInFeet(dist1,dist2){
+    return (Math.abs(dist2-dist1)*264)
+  
+  };
+  
+  function calculatesFarePrice(start, destination){
+    if(distanceTravelledInFeet(start,destination) < 400){
+      return 0;
+    } else if(distanceTravelledInFeet(start,destination) >= 400 && distanceTravelledInFeet(start,destination) <= 2000){
+      return ((distanceTravelledInFeet(start,destination) - 400)*0.02);
+    } else if (distanceTravelledInFeet(start,destination) > 2000 && distanceTravelledInFeet(start,destination) < 2500){
+      return 25;
+    } else if(distanceTravelledInFeet(start,destination) > 2500){
+      return "cannot travel that far";
+    }
+  };
